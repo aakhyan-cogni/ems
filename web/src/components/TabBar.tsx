de8fd7e5fa88b97a0dc38fbdc381198916d28c0b
Sushant Ambekar <SushantSanjay.Ambekar@cognizant.com>
2026-03-17 10:54:23 +0530

    dashboard

--- web/src/components/TabBar.tsx
+++ web/src/components/TabBar.tsx
@@ -3,6 +3,7 @@ import ThemeSwitch from "./ThemeSwitch";
 
 export default function ProfileLayout() {
   const [active, setActive] = useState("dashboard");
+  const [hover,setHover]=useState("");
   const [isLogoutHovered, setIsLogoutHovered] = useState(false);
 
 //   const NavButton = ({ id, label, icon }) => (
@@ -20,7 +21,7 @@ export default function ProfileLayout() {
   return (
     <div className="container-fluid">
       <div className="row min-lg-vh-100">
-        <aside className={`col-12 col-md-3 col-lg-2 border-end bg-body-tertiary px-0 `}>
+        <aside className={`sidebar col-12 col-md-3 col-lg-2 border-end bg-body-tertiary px-0 `}>
           <div className={`d-flex flex-column h-lg-100`}>
             <div className="p-3 border-bottom">
               <h6 className="mb-0">My Account</h6>
@@ -28,47 +29,66 @@ export default function ProfileLayout() {
             </div>
 
             <nav className="nav nav-pills flex-wrap flex-lg-column py-2">
-              <div className="nav-item">
+              <div className="nav-item d-flex">
                 {/* <NavButton id="dashboard" label="Dashboard" icon={"🗓️"}/> */}
                 <button
-                    className={`btn m-1 ms-2 text-start px-3 py-2 ${active === "dashboard" ? "btn-primary" : ""}`}
+                    className={`btn outline-none decoration m-1 mb-0 ms-2 text-start px-3 pt-2 border-none `}
                     onClick={() => setActive("dashboard")}
                     aria-current={active === "dashboard" ? "page" : undefined}
+                    onMouseOver={()=>setHover("dashboard")}
+                    onMouseOut={()=>setHover("")}
                     >
                         <span className="me-2">{"🗓️"}</span> 
                     {"Dashboard"}
                 </button>
+                <div className={`rouded-3 rounded-top rounded-bottom ${active === "dashboard" ? "bg-primary" : ""} my-2`} style={{marginLeft:"auto",width:"5px",backgroundColor:"transparent",color:"transparent"}}>as</div>
               </div>
-              <div className="nav-item">
+                <div className={`  d-none d-lg-block rouded-2 rounded-start rounded-end ${hover === "dashboard" ? "bg-primary" : ""} overflow-hidden ms-3`} style={{width:"70%",height:"2px",backgroundColor:"transparent",color:"transparent"}}>as</div>
+              <div className="nav-item d-flex">
                 {/* <NavButton id="personal" label="Personal Details" icon={"👤"}/> */}
                 <button
-                    className={`btn m-1 ms-2 text-start px-3 py-2 ${active === "personal" ? "btn-primary" : ""}`}
-                    onClick={() => setActive("personal")}>
+                    className={`btn m-1 ms-2 text-start px-3 py-2 border-none`}
+                    onClick={() => setActive("personal")}
+                    onMouseOver={()=>setHover("personal")}
+                    onMouseOut={()=>setHover("")}
+                    >
                         <span className="me-2">{"👤"}</span> 
                     {"Personal Details"}
                 </button>
+                <div className={`rouded-3 rounded-top rounded-bottom ${active === "personal" ? "bg-primary" : ""} my-2`} style={{marginLeft:"auto",width:"5px",backgroundColor:"transparent",color:"transparent"}}>as</div>
               </div>
-              <div className="nav-item">
+                <div className={`d-none d-lg-block ${hover === "personal" ? "bg-primary" : ""} overflow-hidden ms-3`} style={{width:"80%",height:"2px",backgroundColor:"transparent",color:"transparent"}}>as</div>
+              <div className="nav-item d-flex">
                 {/* <NavButton id="payment" label="Payment Details" icon={"💰"}/> */}
                 <button
-                    className={`btn m-1 ms-2 text-start px-3 py-2 ${active === "payment" ? "btn-primary" : ""}`}
-                    onClick={() => setActive("payment")}>
+                    className={`btn m-1 ms-2 text-start px-3 py-2 border-none`}
+                    onClick={() => setActive("payment")}
+                    onMouseOver={()=>setHover("payment")}
+                    onMouseOut={()=>setHover("")}
+                    >
                         <span className="me-2">{"💰"}</span> 
                     {"Payment Details"}
                 </button>
+                <div className={`rouded-3 rounded-top rounded-bottom ${active === "payment" ? "bg-primary" : ""} my-2`} style={{marginLeft:"auto",width:"5px",backgroundColor:"transparent",color:"transparent"}}>as</div>
               </div>
-              <div className="nav-item">
+                <div className={`d-none d-lg-block ${hover === "payment" ? "bg-primary" : ""} overflow-hidden ms-3`} style={{width:"80%",height:"2px",backgroundColor:"transparent",color:"transparent"}}>as</div>
+              <div className="nav-item d-flex">
                 {/* <NavButton id="settings" label="Settings" icon={"⚙️"}/> */}
                 <button
-                    className={`btn m-1 ms-2 text-start px-3 py-2 ${active === "settings" ? "btn-primary" : ""}`}
-                    onClick={() => setActive("settings")}>
+                    className={`btn m-1 ms-2 text-start px-3 py-2 border-none`}
+                    onClick={() => setActive("settings")}
+                    onMouseOver={()=>setHover("settings")}
+                    onMouseOut={()=>setHover("")}
+                    >
                         <span className="me-2">{"💰"}</span> 
                     {"Settings"}
                 </button>
+                <div className={`rouded-3 rounded-top rounded-bottom ${active === "settings" ? "bg-primary" : ""} my-2`} style={{marginLeft:"auto",width:"5px",backgroundColor:"transparent",color:"transparent"}}>as</div>
               </div>
+                <div className={`d-none d-lg-block rounded-lg ${hover === "settings" ? "bg-primary" : ""} overflow-hidden ms-3`} style={{width:"60%",height:"2px",backgroundColor:"transparent",color:"transparent"}}>as</div>
             </nav>
 
-            <div className="mt-auto p-3">
+            <div className="mt-auto p-3 ">
               <button
                 type="button"
                 className={`btn w-100 text-warning ${isLogoutHovered ? "btn-warning text-black" : ""}`}
@@ -99,22 +119,40 @@ export default function ProfileLayout() {
   );
 }
 
-function DashboardContent() {
+function PersonalContent() {
+
+  const [section_icon,setSectionIcon]=useState("success");
+  const [section,setSection]=useState("Basic Profile Info");
+
   return (
-    <>DashBoard</>
+    <div className="container-fluid"  >
+
+      <div className="d-flex" style={{width:"100%",overflow:"hidden"}}>
+          <div className={`bg-${section_icon} rounded-circle mx-2 justify-center align-self-center`} style={{width:"10px",height:"10px",color:"transparent"}}>a</div>
+          <h4 className={`active mt-1 justify-center text-${section_icon}`}>{section}</h4>
+      </div>
+      <hr className={`border-start border-${section_icon} border-2 opacity-95`}></hr>
+
+      <div className="d-flex" style={{width:"100%",height:"100%",overflow:"scroll"}} onScroll={()=>console.log("Scrolled")}>
+        <div className="scroll" style={{width:"99%",height:"100%",overflow:"hidden"}}>
+
+        </div>
+        {/* <input type="range" className="form-control range " style={{width:"1px",writingMode:"vertical-lr",height:"100%",overflow:"hidden"}} /> */}
+      </div>
+    </div>
   );
 }
 
-function PersonalContent() {
+function DashboardContent() {
   return (
-    <>Personal Details</>
+    <>Dashboard</>
   );
 }
 
 function PaymentContent() {
   return (
     <>Payment Details</>
-  );
+  )
 }
 
 function SettingsContent() {
