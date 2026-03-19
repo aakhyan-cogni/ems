ad28dfb318afb3904da2bbfde38ff3b6532e3e02
LakshmiDeepika-10 <lakshmideepika.l@cognizant.com>
2026-03-19 12:07:09 +0530

    Revert "Dashboard/sushant (#10)"
    This reverts commit 0d9478451e8b500c2ef23389e1b64e31cdbc3d48.

--- web/src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-	return (
-		<footer className="p-2 w-100 d-flex align-items-center justify-content-center bg-secondary-subtle z-2">
-			&copy; {new Date().getFullYear()} EMS. All rights reserved.
-		</footer>
-	);
-}
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -7,39 +7,9 @@ import Login from "./pages/login";
 import GlobalEventPage from "./pages/Global_Event";
 import Navbar from "./components/Navbar";
 import "bootstrap";
-import Footer from "./components/Footer";
-import Antigravity from "./components/special/Antigravity";
-import Dashboard from "./pages/Dashboard";
 
 createRoot(document.getElementById("root")!).render(
 	<BrowserRouter>
-		<main className="min-vh-100 min-vw-100 d-flex flex-column justify-content-between">
-			<Antigravity
-				count={200}
-				magnetRadius={6}
-				ringRadius={10}
-				waveSpeed={0.4}
-				waveAmplitude={1}
-				particleSize={1.5}
-				lerpSpeed={0.05}
-				color="#9219fd"
-				autoAnimate
-				particleVariance={1}
-				rotationSpeed={0}
-				depthFactor={1}
-				pulseSpeed={3}
-				particleShape="capsule"
-				fieldStrength={10}
-			/>
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<App />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/events" element={<GlobalEventPage />} />
-				<Route path="/dashboard" element={<Dashboard />} />
-			</Routes>
-			<Footer />
-		</main>
 		<Navbar />
 		<Routes>
 			<Route path="/" element={<App />} />
