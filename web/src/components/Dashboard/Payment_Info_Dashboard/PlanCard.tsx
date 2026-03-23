974e8f12fc5fcdf08a8dbe397f82235e0afef999
Sushant Ambekar <SushantSanjay.Ambekar@cognizant.com>
2026-03-23 15:48:23 +0530

    Dashboard Done

--- web/src/components/Dashboard/Payment_Info_Dashboard/PlanCard.tsx
+++ web/src/components/Dashboard/Payment_Info_Dashboard/PlanCard.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { PricingDetails } from "../../../config/constants";
+import { p } from "motion/react-client";
 
 type props = {
 	active?: boolean;
@@ -6,17 +8,34 @@ type props = {
 };
 
 const PlanCard: React.FC<props> = ({ active, title }) => {
+	const card = PricingDetails[title];
+
 	return (
-		<div className={`m-2 p-2 rounded-5 ${active ? "border border-2 border-primary" : "text-center"}`}>
+		<div className={`m-2 p-2 rounded-5 ${active ? "border border-2 border-primary shadow-lg" : "text-center"}`} style={{
+							background: "linear-gradient(135deg, #0D6EFD, #212529)",
+                            borderImage:"linear-gradient(135deg, #0dcaf0, #212529)"
+						}}>
 			<div
-				className={`${active ? "bg-primary d-flex justify-content-around" : "bg-info text-black "} m-1 rounded-start-5 rounded-end-5 fw-bold py-2 `}
+				className={`${active ? "d-flex justify-content-around" : "bg-info  "} m-1 rounded-start-5 rounded-end-5 fw-bold py-2 `}
 			>
-				<div className={`${(active)?"justify-content-start":""}align-middle `}>{title}</div>
-				{ active &&
-                    <div className="justify-content-end fw-semibold rounded-start-5 rounded-end-5 bg-secondary px-3 py-1">active</div>
-                }
+				<div className={`${active ? "justify-content-start text-light" : ""} align-self-center `}>
+					<h3>{title}</h3>
+				</div>
+				{active && (
+					<div className=" text-light align-self-center justify-content-end fw-lighter rounded-start-5 rounded-end-5 bg-primary px-3 py-1">
+						active
+					</div>
+				)}
 			</div>
-            <div className=" align"></div>
+			<div className="text-center">
+				{/* <h3>{PricingDetails.title.description}</h3> */}
+                <hr className="mx-2 border border-dark opacity-75"/>
+				<p className="py-2 px-1 fw-bold text-light">{card.description}</p>
+
+				{card.features.map((feature, index) => (
+					<p key={index} className="text-light fw-semibold">{feature}</p>
+				))}
+			</div>
 		</div>
 	);
 };
--- web/src/components/Dashboard/Payment_Info_Dashboard/SubscriptionPlans.tsx
+++ web/src/components/Dashboard/Payment_Info_Dashboard/SubscriptionPlans.tsx
@@ -1,38 +1,44 @@
 import React from "react";
 import PlanCard from "./PlanCard";
+import { Cards } from "./SavedPaymentMethods";
 
 const SubscriptionPlans = () => {
-	return (
-		<div className="container-fluid h-100 d-flex flex-column personal-wrapper">
-			{/* Header */}
-			<div className="flex-shrink-0">
-				<div className="d-flex align-items-center w-100 overflow-hidden">
-					<div className="bg-info rounded-circle mx-2" style={{ width: 10, height: 10 }} />
-					<h4 className="mt-1 text-info">Subscription & Plans</h4>
-				</div>
-				<hr className="my-2 border-info border-2 opacity-95" />
+  return (
+    <div className="container-fluid h-100 d-flex flex-column personal-wrapper">
+      {/* Header */}
+      <div className="flex-shrink-0">
+        <div className="d-flex align-items-center w-100 overflow-hidden">
+          <div className="bg-info rounded-circle mx-2" style={{ width: 10, height: 10 }} />
+          <h4 className="mt-1 text-info">Subscription & Plans</h4>
+        </div>
+        <hr className="my-2 border-info border-2 opacity-95" />
+      </div>
+
+      {/* Content */}
+      <div className="flex-grow-1 overflow-y-auto overflow-x-hidden content-pane">
+        <div className="row ">
+
+          {/* Card */}
+          <div className="col-12 col-lg-4">
+            <PlanCard active={true} title="Pro" />
+          </div>
+
+          {/* Card Content */}
+          <div className="col-12 col-lg-7 pb-5 pb-lg-0 flex-column flex-lg-row justify-content-center">
+            <div className="m-2 p-2">
+				<label className="form-label fw-bold fs-4"> Renewal Date </label><br />
+				<label className="form-label fw-semibold fs-5 text-info" >{new Date(Date.now()+(5*24*60*60*1000)).toLocaleDateString()}</label>
+            </div>
+			<div className="alert alert-info" role="alert">
+				🔥 You should <span className="fw-bold text-warning">upgrade</span> your plan to utilize more features
 			</div>
+			<button className="btn btn-danger col-5 me-2 ">Cancle Subscription</button>
+			<button className="btn btn-info col-5 fw-semibold">🔥Upgrade</button>
+          </div>
+        </div>
 
-			<div className="flex-grow-1 overflow-y-auto overflow-x-hidden content-pane">
-				<form className="px-3 pb-3" onSubmit={(e) => e.preventDefault()}>
-					<div className="row g-3">
-						{/* Methods */}
-						<div className="col-12 col-lg-4">
-                            <PlanCard title="Free"/>
-                        </div>
-
-                        <div className="col-12 col-lg-4">
-                            <PlanCard active={true} title="Pro"/>
-                        </div>
-
-                        <div className="col-12 col-lg-4">
-                            <PlanCard title="Super"/>
-                        </div>
-					</div>
-				</form>
-			</div>
-		</div>
-	);
-};
-
+      </div>
+    </div>
+  );
+}
 export default SubscriptionPlans;
