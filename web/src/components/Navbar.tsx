a773fd9ceb9b4c984324eda316e5999d9e4f5a10
aakhyan-cogni <aakhyan.jeyush@cognizant.com>
2026-03-30 11:03:14 +0530

    add profile popover at navbar

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,10 +1,13 @@
-import { Link, NavLink } from "react-router";
+import { Link, NavLink, useNavigate } from "react-router";
 import ThemeSwitch from "./ThemeSwitch";
 import { useLocalDB } from "../store";
 import { Avatar } from "./Avatar";
+import { Popover, OverlayTrigger } from "react-bootstrap";
 
 export default function Navbar() {
-	const { user } = useLocalDB();
+	const { user, setUser } = useLocalDB();
+	const navigate = useNavigate();
+
 	return (
 		<nav className="navbar navbar-expand-lg border-bottom sticky-top bg-body-tertiary flex-nowrap">
 			<div className="container gap-md-2">
@@ -56,8 +59,42 @@ export default function Navbar() {
 			<div className="d-flex align-items-center gap-3 me-5">
 				<ThemeSwitch />
 				{user ? (
-					<div style={{ width: "2rem" }}>
-						<Avatar user={user} />
+					<div style={{ width: "2rem", cursor: "pointer" }}>
+						<OverlayTrigger
+							trigger="click"
+							placement="bottom"
+							rootClose
+							overlay={
+								<Popover id="user-popover" className="shadow border-0">
+									<Popover.Header as="h3" className="text-white border-0">
+										Hi, {user.name.split(" ")[0]}!
+									</Popover.Header>
+									<Popover.Body className="p-0">
+										<div className="list-group list-group-flush">
+											<Link
+												to="/dashboard"
+												className="list-group-item list-group-item-action border-0"
+											>
+												My Dashboard
+											</Link>
+											<button
+												onClick={() => {
+													setUser(null);
+													navigate("/");
+												}}
+												className="list-group-item list-group-item-action text-danger border-0"
+											>
+												Logout
+											</button>
+										</div>
+									</Popover.Body>
+								</Popover>
+							}
+						>
+							<div>
+								<Avatar user={user} />
+							</div>
+						</OverlayTrigger>
 					</div>
 				) : (
 					<Link to="/login" className="btn btn-primary rounded-pill px-4 w-100 w-lg-auto shadow-sm">
